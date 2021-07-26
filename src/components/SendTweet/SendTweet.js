45c56535517e4891564a5f5829f51e46100a5b52
Eduardo Martínez <zamzery@gmail.com>
2021-07-26 13:32:46 -0500

    Inicio de Modal

--- src/components/SendTweet/SendTweet.js
+++ src/components/SendTweet/SendTweet.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Fab} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import moment from 'moment';
+import ModalContainer from '../ModalContainer/ModalContainer';
+
+
+
 import "./SendTweet.scss";
 
 export default function SendTweet() {
+    const [isOpenModal, setIsOpenModal] = useState( false );
+    const openModal = () => {
+        setIsOpenModal( true );
+    }
+    const closeModal = () => {
+        setIsOpenModal( false );
+    }
     return (
         <div className="send-tweet">
             <Fab
                 className="send-tweet__open-modal"
                 color="primary"
                 aria-label="add"
+                onClick={openModal}
             >
                 <AddIcon />
             </Fab>
+            <ModalContainer isOpenModal={isOpenModal} closeModal={closeModal}>
+                <p>Contenido del Modal</p>
+            </ModalContainer>
         </div>
     );
 }
